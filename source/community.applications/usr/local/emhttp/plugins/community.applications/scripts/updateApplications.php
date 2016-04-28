de270e85980e8662b66d6ca39d4676ec1c58b795
Squidly271 <azawadzki@arrowgames.com>
2016-04-28 17:56:03 -0400

    Add notification when auto updating

--- source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/updateApplications.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/updateApplications.php
@@ -15,7 +15,10 @@ function checkPluginUpdate($filename) {
     return false;
   }
 }
-
+function notify($event,$subject,$description,$message="",$type="normal") {
+  $command = '/usr/local/emhttp/plugins/dynamix/scripts/notify -e "'.$event.'" -s "'.$subject.'" -d "'.$description.'" -m "'.$message.'" -i "'.$type.'"';
+  shell_exec($command);
+}
 
 $appList = json_decode(@file_get_contents("/boot/config/plugins/community.applications/AutoUpdate.json"),true);
 if ( ! $appList ) {
@@ -33,6 +36,7 @@ foreach ($pluginsInstalled  as $plugin) {
       exec("logger Auto Updating $plugin");
       exec("/usr/local/emhttp/plugins/dynamix.plugin.manager/scripts/plugin check $plugin");
       exec("/usr/local/emhttp/plugins/dynamix.plugin.manager/scripts/plugin update $plugin");
+      notify("Community Applications","Application Auto Update",$plugin." Automatically Updated");
     } else {
       exec("logger Update available for $plugin - Skipping Auto Update");
     }
