f06b7b85145c5948670b3016319883cded16c9ec
Squidly271 <azawadzki@arrowgames.com>
2016-11-09 18:03:35 -0500

    kill backup

--- source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/deleteDatedBackupSets.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "/usr/local/emhttp/plugins/community.applications/scripts/deleteDatedBackupSets.php & > /dev/null " | at -M NOW >/dev/null 2>&1
-
--- source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/deleteOldBackupSets1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Deleting Old Backup Sets" > /tmp/community.applications/tempFiles/deleteInProgress
-rm -rfv "$1" >> /var/lib/docker/unraid/community.applications.datastore/appdata_backup.log
-rm /tmp/community.applications/tempFiles/deleteInProgress
