29aa0a99764b62b19f37a0815a303d15f9c6073f
Squidly271 <azawadzki@arrowgames.com>
2016-12-31 17:32:42 -0500

    fix permissions

--- copy_to_git.sh
+++ copy_to_git.sh
@@ -2,6 +2,6 @@
 
 mkdir -p "/tmp/GitHub/community.applications/source/community.applications/usr/local/emhttp/plugins/community.applications/"
 
-cp /usr/local/emhttp/plugins/community.applications/* /tmp/GitHub/community.applications/source/community.applications/usr/local/emhttp/plugins/community.applications -R -v
+cp /usr/local/emhttp/plugins/community.applications/* /tmp/GitHub/community.applications/source/community.applications/usr/local/emhttp/plugins/community.applications -R -v -p
 
 
